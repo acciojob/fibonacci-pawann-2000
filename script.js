cdd71d5ffa3903190701a7a84d841c11a35288ba
Pawan <github.3ljwu@ggmaill.aleeas.com>
2024-07-29 17:06:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
-// your code here
+	if (num==0) {
+		return 0;
+	} if (num==1) {
+		return 1;
+	} else {
+		for(i=2;i<number;++i){
+			n3=n1+n2;
+			if (i==num) {
+				return n3;
+			}
+			n1=n2;
+			n2=n3;
+		}
+	}
 }
 
 module.exports = fibonacci;
