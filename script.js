b0ada85013c3a63001d1fdcc7129fc00321fa37a
Pawan <github.3ljwu@ggmaill.aleeas.com>
2024-09-05 05:00:41 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 function fibonacci(num) {
-	if (num==0) {
-		return 0;
-	} if (num==1) {
-		return 1;
-	} else {
-		for(i=2;i<number;++i){
-			n3=n1+n2;
-			if (i==num) {
-				return n3;
-			}
-			n1=n2;
-			n2=n3;
-		}
-	}
+  if (num < 0) {
+    throw new Error('Input must be a non-negative integer.');
+  }
+
+  if (num === 0) return 0;
+  if (num === 1) return 1;
+
+  let a = 0;
+  let b = 1;
+  let c;
+
+  for (let i = 2; i <= num; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+
+  return b;
 }
 
 module.exports = fibonacci;
